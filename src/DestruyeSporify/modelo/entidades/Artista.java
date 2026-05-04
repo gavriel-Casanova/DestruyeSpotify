6e968758cb9df087e9d3537c181e9fbcfe1db8c9
Clau-Martinez <claudia-maribel.martinezga@elorrieta-errekamari.com>
2026-05-04 17:48:08 +0200

    POJOS ALBUM, ARTISTA, CLIENTE MUSICO PODCASTER

--- src/DestruyeSporify/modelo/entidades/Artista.java
+++ src/DestruyeSporify/modelo/entidades/Artista.java
@@ -1,5 +1,57 @@
 package DestruyeSporify.modelo.entidades;
 
 public class Artista {
+	private int idArtiste = 0;
+	private String nombreArtistico = null;
+	// private imagen = null;
+	private String descripcion = null;
+	private String genero = null;
+
+	public int getIdArtiste() {
+		return idArtiste;
+	}
+
+	public void setIdArtiste(int idArtiste) {
+		this.idArtiste = idArtiste;
+	}
+
+	public String getNombreArtistico() {
+		return nombreArtistico;
+	}
+
+	public void setNombreArtistico(String nombreArtistico) {
+		this.nombreArtistico = nombreArtistico;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public String toString() {
+		return "Artista [idArtiste=" + idArtiste + ", nombreArtistico=" + nombreArtistico + ", descripcion="
+				+ descripcion + ", genero=" + genero + "]";
+	}
+
+	public Artista(int idArtiste, String nombreArtistico, String descripcion, String genero) {
+		super();
+		this.idArtiste = idArtiste;
+		this.nombreArtistico = nombreArtistico;
+		this.descripcion = descripcion;
+		this.genero = genero;
+	}
+	
 
 }
--- src/DestruyeSporify/modelo/entidades/Podcaster.java
+++ src/DestruyeSporify/modelo/entidades/Podcaster.java
@@ -2,4 +2,9 @@ package DestruyeSporify.modelo.entidades;
 
 public class Podcaster extends Artista {
 
+	public Podcaster(int idArtiste, String nombreArtistico, String descripcion, String genero) {
+		super(idArtiste, nombreArtistico, descripcion, genero);
+		// TODO Auto-generated constructor stub
+	}
+
 }
